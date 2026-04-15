06538e0d8a71de0a476ad5af1327253403e792d2
Nix Hartogh <phenix@phenixhartogh.com.au>
2026-04-15 13:36:33 +1000

    Fix cURL options formatting in send.php

--- send.php
+++ send.php
@@ -1,18 +1,30 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(["error" => "Method not allowed"]);
+    exit();
+}
 
 $data = file_get_contents("php://input");
 
 $ch = curl_init("https://www.5centsms.com.au/api/v5/sms");
 
 curl_setopt_array($ch, [
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_POST => true,
-CURLOPT_HTTPHEADER => ["Content-Type: application/json"],
-CURLOPT_POSTFIELDS => $data
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_HTTPHEADER => ["Content-Type: application/json"],
+    CURLOPT_POSTFIELDS => $data
 ]);
 
 $response = curl_exec($ch);
